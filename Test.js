ea91370b186ed30236f07f41fc31c2381cec6b2b
Kleber Vales <ksvales@outlook.pt>
2024-09-27 10:19:10 -0300

    Enviando resolução

--- Test.js
+++ Test.js
@@ -1,6 +1,27 @@
-console.log('Imprimir algo');
+// console.log('Imprimir algo');
 
-let varr = 11;
+// let varr = 11;
 
-console.log(varr);
+// const varr2 = 12;
+
+const prompt = require("prompt-sync")();
+
+let eta = prompt("preço do etanol: ");
+let gaso = prompt("preço do gasolina: ");
+let tipo = prompt("tipo de combustivel do carro: ")
+let kmPorLitro = prompt("km por litro: ");
+let distancia = prompt("distância percorrida: ");
+
+if(tipo == "etanol"){
+    console.log(distancia/kmPorLitro * eta);
+} else{
+    console.log(distancia/kmPorLitro * gaso);
+}
+
+
+
+
+
+
+
 
